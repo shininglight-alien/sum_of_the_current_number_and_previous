a6349a90516a4f84ce78f55c08d13ad5a714c015
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 09:32:55 +0800

    revised

--- sum_of_the_current_number_and_previous.py
+++ sum_of_the_current_number_and_previous.py
@@ -1,10 +1,11 @@
 # Print the sum of the current number and the previous number
 
 def sum_of_numbers():
+    numbers = list(range(1, 11))
     prev_num = 0
-    for i in range(1, 11):
-        sum = prev_num + i
-        print("Current Number:", i, "Previous Number:", prev_num, " Sum: ", sum)
-        prev_num = i
+    for num in numbers:
+        sum = prev_num + num
+        print("Current Number:", num, "Previous Number:", prev_num, " Sum: ", sum)
+        prev_num = num
 
 sum_of_numbers()
